d2ef41dbb9651c8b3060235a936c0d0d7abe5cbb
Zan Skamljic <zan.skamljic@gmail.com>
2022-12-18 11:46:24 +0100

    feat: add day 16 part 2

--- src/Day16.java
+++ src/Day16.java
@@ -1,79 +1,85 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Day16 {
-    private static final int MAX_TICK = 30;
+import static java.util.Collections.disjoint;
 
+public class Day16 {
     public static void main(String[] args) throws IOException {
-        var input = Arrays.asList("""
-            Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-            Valve BB has flow rate=13; tunnels lead to valves CC, AA
-            Valve CC has flow rate=2; tunnels lead to valves DD, BB
-            Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-            Valve EE has flow rate=3; tunnels lead to valves FF, DD
-            Valve FF has flow rate=0; tunnels lead to valves EE, GG
-            Valve GG has flow rate=0; tunnels lead to valves FF, HH
-            Valve HH has flow rate=22; tunnel leads to valve GG
-            Valve II has flow rate=0; tunnels lead to valves AA, JJ
-            Valve JJ has flow rate=21; tunnel leads to valve II
-            """.split("\n"));
-        var input2 = Files.readAllLines(Path.of("input16.txt"));
-        input = input2;
+        var input = Files.readAllLines(Path.of("input16.txt"));
 
         var valves = input.stream()
             .map(Valve::parse)
             .collect(Collectors.toMap(Valve::name, Function.identity()));
-
-        part1(valves);
-    }
-
-    private static void part1(Map<String, Valve> valves) {
-        var nonEmpty = valves.values()
-            .stream()
-            .filter(v -> v.rate != 0)
-            .map(Valve::name)
-            .toList();
         var distances = findDistances(valves);
 
-        var pressure = valves.entrySet()
+        part1(distances, valves);
+        part2(distances, valves);
+    }
+
+    private static void part1(Map<String, Integer> distances, Map<String, Valve> valves) {
+        var result = new Walker(distances, valves).recurse(Set.of(), valves.get("AA"), 30, 0, new HashMap<>())
+            .values()
             .stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().rate));
-        var result = recurse(distances, pressure, 0, 0, 0, "AA", nonEmpty);
+            .mapToInt(i -> i)
+            .max()
+            .orElseThrow();
         System.out.println(result);
     }
 
-    static int recurse(Map<String, Integer> distances, Map<String, Integer> pressures, int pressure, int pressurePerTick, int currentTick, String location, List<String> paths) {
-        var max = pressure + (MAX_TICK - currentTick) * pressurePerTick;
+    private static void part2(Map<String, Integer> distances, Map<String, Valve> valves) {
+        var result = new Walker(distances, valves).recurse(Set.of(), valves.get("AA"), 26, 0, new HashMap<>());
 
-        for (var path : paths) {
-            var moveAndOpen = distances.get(location + path) + 1;
-            if (currentTick + moveAndOpen >= MAX_TICK) continue;
-
-            var tick = currentTick + moveAndOpen;
-            var newPressure = pressure + moveAndOpen * pressurePerTick;
-            var newPressurePerTick = pressurePerTick + pressures.get(path);
-            var candidate = recurse(distances, pressures, newPressure, newPressurePerTick, tick, path, pathsWithoutSome(paths, path));
-            if (candidate > max) {
-                max = candidate;
-            }
-        }
-        return max;
+        var best = result.entrySet()
+            .stream()
+            .flatMapToInt(e ->
+                result.entrySet().stream()
+                    .filter(e2 -> disjoint(e.getKey(), e2.getKey()))
+                    .mapToInt(e2 -> e.getValue() + e2.getValue())
+            )
+            .max()
+            .orElseThrow();
+        System.out.println(best);
     }
 
-    private static List<String> pathsWithoutSome(List<String> paths, String path) {
-        var result = new ArrayList<>(paths);
-        result.remove(path);
-        return result;
+    static class Walker {
+        private final Map<String, Integer> distances;
+        private final List<Valve> openable;
+
+        public Walker(Map<String, Integer> distances, Map<String, Valve> valves) {
+            this.distances = distances;
+            openable = valves.values()
+                .stream()
+                .filter(v -> v.rate != 0)
+                .toList();
+        }
+
+        Map<Set<Valve>, Integer> recurse(Set<Valve> open, Valve current, int timeRemaining, int totalFlow, Map<Set<Valve>, Integer> bestSeen) {
+            bestSeen.merge(open, totalFlow, Math::max);
+            for (var candidate : openable) {
+                int timeAfter = timeRemaining - distances.get(current.name + candidate.name) - 1;
+                if (open.contains(candidate) || timeAfter <= 0) continue;
+
+                recurse(join(candidate, open), candidate, timeAfter, timeAfter * candidate.rate + totalFlow, bestSeen);
+            }
+            return bestSeen;
+        }
+
+        private Set<Valve> join(Valve element, Set<Valve> s) {
+            var ss = new HashSet<>(s);
+            ss.add(element);
+            return ss;
+        }
     }
 
     private static Map<String, Integer> findDistances(Map<String, Valve> valves) {
